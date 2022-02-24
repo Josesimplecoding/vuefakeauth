e7130263e01ae3344c14649f7096907a745457c8
Josesimplecoding <Jose.salazar9877@gmail.com>
2022-02-24 13:59:18 -0600

    Added Secret Page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -14,6 +14,9 @@
                     <router-link to="/login">
                         <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">Login</li>
                     </router-link>
+                        <router-link to="/secret">
+                        <li class="px-4 py-8 hover:cursor-pointer hover:bg-indigo-500 hover:text-indigo-800">Secret</li>
+                    </router-link>
                 </ul>
             </nav>
         </div>
